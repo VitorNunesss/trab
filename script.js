007eb2fc686426ab087bb4ba435d940b587f26be
VitorNunesss <168797085+VitorNunesss@users.noreply.github.com>
2024-10-02 20:17:41 -0300

    script.js

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
-const characterGrid = document.getElementById('characterGrid');
-const searchInput = document.getElementById('searchInput');
-const favoriteFilterButton = document.getElementById('favoriteFilter');
-const speciesFilter = document.getElementById('speciesFilter');
-const statusFilter = document.getElementById('statusFilter');
+const gradePersonagens = document.getElementById('characterGrid');
+const entradaBusca = document.getElementById('searchInput');
+const botaoFiltroFavoritos = document.getElementById('favoriteFilter');
+const filtroEspecie = document.getElementById('speciesFilter');
+const filtroStatus = document.getElementById('statusFilter');
 
-let currentPage = 1;   
-let isLoading = false;  
-let favorites = JSON.parse(localStorage.getItem('favorites')) || []; 
-let showFavoritesOnly = false;
-let allCharacters = []; 
-let renderedCharacterIds = new Set(); 
+let paginaAtual = 1;   
+let carregando = false;  
+let favoritos = JSON.parse(localStorage.getItem('favorites')) || []; 
+let mostrarFavoritosApenas = false;
+let todosOsPersonagens = []; 
+let idsPersonagensRenderizados = new Set(); 
 
-function saveFavorites() {
-    localStorage.setItem('favorites', JSON.stringify(favorites));
+function salvarFavoritos() {
+    localStorage.setItem('favorites', JSON.stringify(favoritos));
 }
 
-function toggleFavorite(id) {
-    const index = favorites.indexOf(id); 
-    if (index > -1) {
-        favorites.splice(index, 1); 
+function alternarFavorito(id) {
+    const indice = favoritos.indexOf(id); 
+    if (indice > -1) {
+        favoritos.splice(indice, 1); 
     } else {
-        favorites.push(id); 
+        favoritos.push(id); 
     }
-    saveFavorites(); 
-    renderCharacters(); 
+    salvarFavoritos(); 
+    renderizarPersonagens(); 
 }
 
-function translateSpecies(species) {
-    const speciesTranslation = {
+function traduzirEspecie(especie) {
+    const traducaoEspecie = {
         "Human": "Humano",
         "Alien": "Alienígena",
     };
-    return speciesTranslation[species] || species; 
+    return traducaoEspecie[especie] || especie; 
 }
 
-function translateStatus(status) {
-    const statusTranslation = {
+function traduzirStatus(status) {
+    const traducaoStatus = {
         "Alive": "Vivo",
         "Dead": "Morto",
         "unknown": "Desconhecido"
     };
-    return statusTranslation[status] || status; 
+    return traducaoStatus[status] || status; 
 }
 
-function createCharacterCard(character) {
-    const characterCard = document.createElement('div'); 
-    characterCard.classList.add('character-card'); 
+function criarCartaoPersonagem(personagem) {
+    const cartaoPersonagem = document.createElement('div'); 
+    cartaoPersonagem.classList.add('character-card'); 
 
-    const favoriteClass = favorites.includes(character.id) ? 'favorited' : '';
+    const classeFavorito = favoritos.includes(personagem.id) ? 'favorited' : '';
 
-    characterCard.innerHTML = `
-        <img src="${character.image}" alt="${character.name}">
-        <h3>${character.name}</h3>
-        <span class="favorite ${favoriteClass}" data-id="${character.id}">★</span>
+    cartaoPersonagem.innerHTML = `
+        <img src="${personagem.image}" alt="${personagem.name}">
+        <h3>${personagem.name}</h3>
+        <span class="favorite ${classeFavorito}" data-id="${personagem.id}">★</span>
         <div class="character-details">
-            <p>Status: ${translateStatus(character.status)}</p>
-            <p>Espécie: ${translateSpecies(character.species)}</p>
+            <p>Status: ${traduzirStatus(personagem.status)}</p>
+            <p>Espécie: ${traduzirEspecie(personagem.species)}</p>
         </div>
     `;
 
-    characterCard.querySelector('.favorite').onclick = () => toggleFavorite(character.id);
+    cartaoPersonagem.querySelector('.favorite').onclick = () => alternarFavorito(personagem.id);
     
-    characterGrid.appendChild(characterCard); 
+    gradePersonagens.appendChild(cartaoPersonagem); 
 }
 
-function fetchCharacterById(id) {
+function buscarPersonagemPorId(id) {
     return fetch(`https://rickandmortyapi.com/api/character/${id}`)
         .then(response => response.json())
         .catch(error => {
@@ -72,99 +72,93 @@ function fetchCharacterById(id) {
         });
 }
 
-function fetchCharacters(page) {
-    if (isLoading) return; 
-    isLoading = true; 
+function buscarPersonagens(pagina) {
+    if (carregando) return; 
+    carregando = true; 
 
-    fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+    fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
         .then(response => response.json())
         .then(data => {
-            allCharacters = [...allCharacters, ...data.results];
-            renderFilteredCharacters(allCharacters);
-            isLoading = false;  
+            todosOsPersonagens = [...todosOsPersonagens, ...data.results]; 
+            renderizarPersonagensFiltrados(todosOsPersonagens); 
+            carregando = false;  
         })
         .catch(error => {
             console.log(error); 
-            isLoading = false; 
+            carregando = false; 
         });
 }
 
-function fetchSearchedCharacters(query) {
+function buscarPersonagensPorBusca(query) {
     fetch(`https://rickandmortyapi.com/api/character/?name=${query}`)
         .then(response => response.json())
         .then(data => {
-            characterGrid.innerHTML = ''; 
-            data.results.forEach(createCharacterCard); 
+            gradePersonagens.innerHTML = ''; 
+            data.results.forEach(criarCartaoPersonagem); 
         })
         .catch(error => {
             console.log('Nenhum personagem encontrado', error); 
-            characterGrid.innerHTML = '<p>Nenhum personagem encontrado</p>';
+            gradePersonagens.innerHTML = '<p>Nenhum personagem encontrado</p>';
         });
 }
 
-function filterCharacters(characters) {
-    const species = speciesFilter.value; 
-    const status = statusFilter.value; 
+function filtrarPersonagens(personagens) {
+    const especie = filtroEspecie.value; 
+    const status = filtroStatus.value; 
 
-    return characters.filter(character => {
-        const speciesMatch = species ? character.species === species : true;
-        const statusMatch = status ? character.status === status : true;
-        return speciesMatch && statusMatch;
+    return personagens.filter(personagem => {
+        const correspondeEspecie = especie ? personagem.species === especie : true;
+        const correspondeStatus = status ? personagem.status === status : true;
+        return correspondeEspecie && correspondeStatus;
     });
 }
 
-function renderFilteredCharacters(characters) {
-    const filteredCharacters = filterCharacters(characters);
-    filteredCharacters.forEach(character => {
-        // Verifica se o ID já foi renderizado
-        if (!renderedCharacterIds.has(character.id)) {
-            createCharacterCard(character);
-            renderedCharacterIds.add(character.id); 
+function renderizarPersonagensFiltrados(personagens) {
+    const personagensFiltrados = filtrarPersonagens(personagens);
+    personagensFiltrados.forEach(personagem => {
+        if (!idsPersonagensRenderizados.has(personagem.id)) {
+            criarCartaoPersonagem(personagem);
+            idsPersonagensRenderizados.add(personagem.id); 
         }
     });
 }
 
-function renderCharacters() {
-    characterGrid.innerHTML = ''; 
-    renderedCharacterIds.clear();
-    if (showFavoritesOnly) {
-        const filteredFavorites = favorites.map(fetchCharacterById); 
-        Promise.all(filteredFavorites).then(renderFilteredCharacters);
+function renderizarPersonagens() {
+    gradePersonagens.innerHTML = ''; 
+    idsPersonagensRenderizados.clear(); 
+    if (mostrarFavoritosApenas) {
+        const favoritosFiltrados = favoritos.map(buscarPersonagemPorId); 
+        Promise.all(favoritosFiltrados).then(renderizarPersonagensFiltrados);
     } else {
-        fetchCharacters(currentPage); 
+        buscarPersonagens(paginaAtual); 
     }
 }
 
-
-searchInput.addEventListener('input', (e) => {
+entradaBusca.addEventListener('input', (e) => {
     const query = e.target.value.trim(); 
-    characterGrid.innerHTML = ''; 
-    query.length ? fetchSearchedCharacters(query) : renderCharacters(); 
+    gradePersonagens.innerHTML = ''; 
+    query.length ? buscarPersonagensPorBusca(query) : renderizarPersonagens(); 
 });
 
-
-favoriteFilterButton.addEventListener('click', () => {
-    showFavoritesOnly = !showFavoritesOnly; 
-    favoriteFilterButton.textContent = showFavoritesOnly ? 'Mostrar Todos' : 'Mostrar Favoritos';
-    renderCharacters();
+botaoFiltroFavoritos.addEventListener('click', () => {
+    mostrarFavoritosApenas = !mostrarFavoritosApenas; 
+    botaoFiltroFavoritos.textContent = mostrarFavoritosApenas ? 'Mostrar Todos' : 'Mostrar Favoritos';
+    renderizarPersonagens();
 });
 
-
-speciesFilter.addEventListener('change', () => {
-    renderCharacters(); 
+filtroEspecie.addEventListener('change', () => {
+    renderizarPersonagens(); 
 });
 
-statusFilter.addEventListener('change', () => {
-    renderCharacters(); 
+filtroStatus.addEventListener('change', () => {
+    renderizarPersonagens(); 
 });
-
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight - 100 && !isLoading) {
-        currentPage++; 
-        fetchCharacters(currentPage); 
+    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight - 100 && !carregando) {
+        paginaAtual++; 
+        buscarPersonagens(paginaAtual); 
     }
 });
 
-
-renderCharacters();
+renderizarPersonagens();
